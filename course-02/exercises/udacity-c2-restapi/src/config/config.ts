f642f77af01aaa60697b604b48d856ff283f3e68
Ismail Al Kamal <ismail@nawatt.com>
2022-08-13 23:04:37 +0300

    revised models due to sequelize error

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,22 +1,22 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: "postgres_ismail",
+    password: "postgres_ismail",
+    database: "postgres_ismail",
+    host: "postgres-ismail.comcxmlcotuc.us-east-1.rds.amazonaws.com",
+    dialect: "postgres",
+    aws_region: "us-east-1",
+    aws_profile: "default",
+    aws_media_bucket: "udacity-cloud-ismail-2022",
   },
-  "jwt": {
-    "secret": " "
+  jwt: {
+    secret: "twisttwist",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+};
